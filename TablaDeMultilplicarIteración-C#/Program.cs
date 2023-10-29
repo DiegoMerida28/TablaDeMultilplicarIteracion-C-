a2e32927ec1a6ed18e38bb270fb9991b1d416b61
DiegoMerida28 <145516421+DiegoMerida28@users.noreply.github.com>
2023-10-28 21:53:55 -0600

    Agregar archivos de proyecto.

---
+++ TablaDeMultilplicarIteración-C#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número para generar la tabla de multiplicar: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Tabla de multiplicar del " + numero + ":");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = numero * i;
+            Console.WriteLine(numero + " x " + i + " = " + resultado);
+        }
+    }
+}
